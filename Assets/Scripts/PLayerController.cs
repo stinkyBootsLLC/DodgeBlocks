d289d956adb628310a82f093b5c0602d84994db4
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2017-05-07 15:55:21 -0400

    virtual joystick
    added cross platform input controller
    standard asset

--- Assets/Scripts/PLayerController.cs
+++ Assets/Scripts/PLayerController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;  // allow us to us the virtual joystick
 
 public class PLayerController : MonoBehaviour 
 {
-	
 	[SerializeField] float speed = 0.5f;
 	[SerializeField] float mapWidth = 3.0f;
+
 
 	private GameController gameController;
 
@@ -29,7 +30,7 @@ public class PLayerController : MonoBehaviour
 	void PlayerMovement ()
 	{
 		// horizontal inputs
-		float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+		float x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
 		Vector2 newPosition =  rb.position + Vector2.right * x;
 
@@ -39,13 +40,6 @@ public class PLayerController : MonoBehaviour
 
 		rb.MovePosition(newPosition);
 
-//		#if UNITY_ANDROID
-//		if (Input.touchCount == 1)
-//		{
-//			transform.Translate (Input.touches[0].deltaPosition.x * 1f, 0 , 0);
-//		}
-//
-//		#endif
 
 	}
 
